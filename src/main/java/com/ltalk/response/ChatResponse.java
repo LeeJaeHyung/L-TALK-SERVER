219ce8f77e21b6a1f9d14597d8164f84bebd23cd
LeeJaeHyung <leejh7623@gmail.com>
2024-10-31 20:00:32 +0900

    feat 채팅방 + 채팅 전송 및 저장 로직 작성완료

--- src/main/java/com/ltalk/response/ChatResponse.java
+++ src/main/java/com/ltalk/response/ChatResponse.java
@@ -3,14 +3,18 @@ package com.ltalk.response;
 import com.ltalk.request.ChatRequest;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ChatResponse {
     private String receiver;
     private String sender;
     private String message;
+    private LocalDateTime sendDate;
     public ChatResponse(ChatRequest chatRequest) {
         this.receiver = chatRequest.getReceiver();
         this.sender = chatRequest.getSender();
         this.message = chatRequest.getMessage();
+        this.sendDate = chatRequest.getSendDate();
     }
 }
